66341cce0e97b7f2488ebd56774c7c68649f5346
Work <Work@MSI>
2024-10-01 16:11:48 -0500

    Where we ended on Oct 1 - resume with creating a `std::queue` object

---
+++ Beginning topic 2/Beginning topic 2.cpp
@@ -0,0 +1,29 @@
+// Beginning topic 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+#include<queue> //queue (line) 
+
+
+using std::cout, std::string, std::vector;
+
+//abstract classes have at least one "pure" virtual function
+
+class LinkedList
+{
+	//add some member vars
+
+	//add some algorithms (AKA: functions) that allow manipulation 
+	//of the linked list object 
+	void insertAtFrontOfList(); 
+
+	void insertAtBackOfList(); 
+
+};
+
+int main()
+{
+
+}
