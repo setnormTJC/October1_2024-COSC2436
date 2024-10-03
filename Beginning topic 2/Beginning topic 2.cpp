115a0a9a3756419806075ef37bdfc76eb29754dc
Work <Work@MSI>
2024-10-03 16:14:10 -0500

    Where we ended on Oct 3 - resume with Topic 2, pt 2 (writing our OWN static and dynamic array classes)

--- Beginning topic 2/Beginning topic 2.cpp
+++ Beginning topic 2/Beginning topic 2.cpp
@@ -6,11 +6,17 @@
 
 #include<queue> //queue (line) 
 
+#include<map> 
+
+//std::priority_queue
+#include<list> 
+
 
 using std::cout, std::string, std::vector;
 
 //abstract classes have at least one "pure" virtual function
 
+/*we will do this later ...*/
 class LinkedList
 {
 	//add some member vars
@@ -23,7 +29,60 @@ class LinkedList
 
 };
 
+class Stack
+{
+	//member vars:
+
+	//add some member functions (algorithms for "common" processes) 
+};
+
+
 int main()
 {
-	//
+	std::list<int> someListNums; 
+	//someListNums.pus
+	std::vector <string> someNames =
+	{
+		"Bob",
+		"Alice",
+		"Darth"
+	};
+
+	//for (auto& name : someNames)
+	//{
+	//	cout << name << "\n";
+	//}
+	
+	std::queue <string> lane1AtGroceryStore; 
+	lane1AtGroceryStore.push("Bob"); 
+	lane1AtGroceryStore.push("Boboe");
+	lane1AtGroceryStore.push("Alice");
+	lane1AtGroceryStore.push("Darth");
+
+
+
+	//print the contents of the queue? 
+	//for (auto& personAtGroceryStore : lane1AtGroceryStore) //doesn't work!
+	//{
+
+	//}
+	//lane1AtGroceryStore.print
+	//for (int i = 0; i < lane1AtGroceryStore.size(); ++i)
+	//{
+	while (!lane1AtGroceryStore.empty())
+	{
+		cout << lane1AtGroceryStore.front() << " ";
+		lane1AtGroceryStore.pop(); 
+	}
+
+	//}
+/*	{
+		"Bob", 
+		"Alice", 
+		"Darth"
+	};*/ //"initializer list syntax" in C++
+	
+	//lane1AtGroceryStore.
+
+	//std::map
 }
